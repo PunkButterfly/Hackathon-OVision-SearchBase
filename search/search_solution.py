d57e0e98b565a1b43598b1ad136ee8f8eebc673d
Илья <ilyasolonitsyn@mail.ru>
2022-04-16 23:52:41 +0300

    VP Tree

--- search/search_solution.py
+++ search/search_solution.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 from typing import List, Tuple
 from config import Config as cfg
 from .search import Base
-import faiss
+from sklearn.cluster import MiniBatchKMeans
+import nmslib
 
 
 class SearchSolution(Base):
@@ -33,23 +34,17 @@ class SearchSolution(Base):
 
         self.reg_matrix = np.concatenate(self.reg_matrix, axis=0)
 
-        self.reg_matrix = self.reg_matrix.astype('float32')
         self.pass_dict = data['pass']
 
         print("Building model...")
-        self.quantiser = faiss.IndexFlatL2(self.dim)
-        self.faiss_model = faiss.IndexIVFFlat(self.quantiser, self.dim,
-                                              self.n_clusters, faiss.METRIC_INNER_PRODUCT)
-
-        self.faiss_model.train(self.reg_matrix)
-        self.faiss_model.add(self.reg_matrix)
+        self.index = nmslib.init(method='vptree', space='cosinesimil')
+        self.index.addDataPointBatch(self.reg_matrix)
+        self.index.createIndex(print_progress=True)
         print("Model build")
 
     def search(self, query: np.array) -> List[Tuple]:
-        D, I = self.faiss_model.search(np.array([query.astype('float32')]), self.top_n)
-
-        # similarity = self.cos_sim(query, response_indexes)
-        return [(I[0][i], D[0][i]) for i in range(self.top_n)]
+        ids, distances = self.index.knnQuery(query, k=1)
+        return [(ids[0], distances[0])]
 
     def cos_sim(self, query: np.array, response_indexes) -> np.array:
         pass
